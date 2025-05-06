2f0a7a0bdf40d8fb1d4ec317ab89801bb96348df
hoainb1303 <hoainb1303@gmail.com>
2025-05-07 00:34:37 +0700

    Install TailwindCSS and add Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Search from "./components/search";
 
 const App = () => {
-  return <div>App</div>;
+  const [searchTerm, setSearchTerm] = React.useState("");
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner"></img>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/components/search.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="./search.svg" alt="Search" />
+        <input
+          type="text"
+          placeholder="Search through thoudsands of movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="search-input"
+        ></input>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
