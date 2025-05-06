aabfa17d0df30c9ea8a6f4e5db2604213907bed4
hoainb1303 <hoainb1303@gmail.com>
2025-05-06 23:27:42 +0700

    First study day

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-const Card = ({ title }) => {
-  return (
-    <div>
-      <h2>{title}</h2>
-    </div>
-  );
-};
+import React from "react";
 
 const App = () => {
-  return (
-    <div className="card-container">
-      <Card title="Movie 1" /> <Card title="Movie 2" /> <Card title="Movie 3" />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
